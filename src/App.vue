0a2618eaaf48d05a081de5cb588c10a43fa53519
emit07 <adv.deleo@gmail.com>
2021-03-03 20:43:01 -0800

    added form, api, display, some stats for basic country stats

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,5 @@
 <template>
-  <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </div>
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma@0.9.1/css/bulma.min.css">
   <router-view/>
 </template>
 
@@ -11,20 +8,13 @@
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
+  display: flex;
   text-align: center;
+  justify-content: center;
+  min-height: 100vh;
+  max-height: 120vh;
   color: #2c3e50;
+  margin: 60px;
 }
 
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
 </style>
